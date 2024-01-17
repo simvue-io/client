db14fff1de98e5c7d38b442ea390e3f676143b7e
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-01-17 22:42:25 +0000

    Minor adjustments

---
+++ tests/functional/test_run_offline_events.py
@@ -0,0 +1,56 @@
+import configparser
+import filecmp
+import os
+import shutil
+import time
+import unittest
+import uuid
+from simvue import Run, Client
+from simvue.sender import sender
+
+import common
+
+
+class TestRunOfflineEvents(unittest.TestCase):
+    def test_run_events(self):
+        """
+        Try logging events and retrieving them from an offline run
+        """
+        common.update_config()
+        try:
+            shutil.rmtree("./offline")
+        except:
+            pass
+
+        name = "test-%s" % str(uuid.uuid4())
+        folder = "/test-%s" % str(uuid.uuid4())
+        run = Run("offline")
+        run.init(name, folder=folder)
+
+        run.log_event("test-event-1", timestamp="2022-01-03 16:42:30.849617")
+        run.log_event("test-event-2", timestamp="2022-01-03 16:42:31.849617")
+
+        run.close()
+
+        sender()
+
+        time.sleep(5)
+
+        client = Client()
+        data = client.get_runs([f"name == {name}"])
+        run_id = data[0]["id"]
+
+        data = client.get_events(run_id)
+
+        data_compare = [
+            {"timestamp": "2022-01-03 16:42:30.849617", "message": "test-event-1"},
+            {"timestamp": "2022-01-03 16:42:31.849617", "message": "test-event-2"},
+        ]
+
+        self.assertEqual(data, data_compare)
+
+        runs = client.delete_folder(folder, runs=True)
+
+
+if __name__ == "__main__":
+    unittest.main()
