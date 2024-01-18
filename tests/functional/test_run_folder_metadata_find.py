6256a165005cb56dfea02a73b17ab23f032bbb68
Andrew Lahiff <andrew.lahiff@gmail.com>
2024-01-18 08:49:30 +0000

    Use random metadata key name & value to ensure test is more reliable

--- tests/functional/test_run_folder_metadata_find.py
+++ tests/functional/test_run_folder_metadata_find.py
@@ -1,24 +1,28 @@
+import random
 import unittest
 import uuid
 from simvue import Run, Client
+
 
 class TestRunFolderMetadataFind(unittest.TestCase):
     def test_run_folder_metadata_find(self):
         """
         Create a run & folder with metadata, find it then delete it
         """
-        name = 'test-%s' % str(uuid.uuid4())
-        folder = '/tests/%s' % str(uuid.uuid4())
+        name = "test-%s" % str(uuid.uuid4())
+        folder = "/tests/%s" % str(uuid.uuid4())
+        key = str(uuid.uuid4())
+        metadata = {key: 100 * random.random()}
         run = Run()
         run.init(name, folder=folder)
-        run.set_folder_details(path=folder, metadata={'atest': 5.0})
+        run.set_folder_details(path=folder, metadata=metadata)
         run.close()
 
         client = Client()
-        data = client.get_folders(['atest == 5.0'])
+        data = client.get_folders([f"metadata.{key} == {metadata[key]}"])
         found = False
         for item in data:
-            if item['path'] == folder:
+            if item["path"] == folder:
                 found = True
         self.assertTrue(found)
 
@@ -28,7 +32,8 @@ class TestRunFolderMetadataFind(unittest.TestCase):
         with self.assertRaises(Exception) as context:
             client.get_folder(folder)
 
-        self.assertTrue('Folder does not exist' in str(context.exception))
+        self.assertTrue("Folder does not exist" in str(context.exception))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
